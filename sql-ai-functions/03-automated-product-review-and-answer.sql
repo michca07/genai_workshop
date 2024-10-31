75a447dc9a6a97eb412748dac9423f0ce2c4aa38
cchalc <cchalc>
2024-10-31 06:20:06 +0000

    adding sql functions

---
+++ sql-ai-functions/03-automated-product-review-and-answer.sql
@@ -0,0 +1,207 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC # 3/ Automated product review and classification with SQL functions
+-- MAGIC
+-- MAGIC
+-- MAGIC In this demo, we will explore the SQL AI function `ai_query` to create a pipeline extracting product review information.
+-- MAGIC
+-- MAGIC <img src="https://raw.githubusercontent.com/databricks-demos/dbdemos-resources/main/images/product/sql-ai-functions/sql-ai-query-function-flow.png" width="1000">
+-- MAGIC
+-- MAGIC **Don't forget about built-in SQL AI functions!** *In this notebook, we show you how to create your own custom functions. However, many text-related tasks (translation, classification etc.) are available as [builtin SQL functions]($./01-Builtin-SQL-AI-Functions). If you can, prefere these as they're easy to use and performant!*
+-- MAGIC
+-- MAGIC <!-- Collect usage data (view). Remove it to disable collection or disable tracker during installation. View README for more details.  -->
+-- MAGIC <img width="1px" src="https://ppxrzfxige.execute-api.us-west-2.amazonaws.com/v1/analytics?category=DBSQL&org_id=1444828305810485&notebook=%2F03-automated-product-review-and-answer&demo_name=sql-ai-functions&event=VIEW&path=%2F_dbdemos%2FDBSQL%2Fsql-ai-functions%2F03-automated-product-review-and-answer&version=1">
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC To run this notebook, connect to <b> SQL endpoint </b>. The AI_QUERY function is available on Databricks SQL Pro and Serverless.
+
+-- COMMAND ----------
+
+-- as previously, make sure you run this notebook using a SQL Warehouse (not a cluster)
+SELECT assert_true(current_version().dbsql_version is not null, 'YOU MUST USE A SQL WAREHOUSE');
+
+USE CATALOG users;
+USE SCHEMA christopher_chalcraft;
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC
+-- MAGIC ## Simplifying AI function access for SQL users (*CHANGE THIS IMAGE)
+-- MAGIC
+-- MAGIC As reminder, `ai_query` signature is the following:
+-- MAGIC
+-- MAGIC ```
+-- MAGIC SELECT ai_query(<Endpoint Name>, <prompt>)
+-- MAGIC ```
+-- MAGIC
+-- MAGIC In the [previous notebook]($./02-Generate-fake-data-with-AI-functions-Foundation-Model), we created a wrapper `ASK_LLM_MODEL` function to simplify our SQL operation and hide the configuration details to end-users. We will re-use this function for this pipeline.
+-- MAGIC
+-- MAGIC In order to simplify the user-experience for our analysts, we will build prescriptive SQL functions that ask natural language questions of our data and return the responses as structured data.
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Review our raw data
+SELECT * FROM fake_reviews INNER JOIN fake_customers using (customer_id)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Review analysis with prompt engineering 
+-- MAGIC &nbsp;
+-- MAGIC The keys to getting useful results back from a LLM model are:
+-- MAGIC - Asking it a well-formed question
+-- MAGIC - Being specific about the type of answer that you are expecting
+-- MAGIC
+-- MAGIC In order to get results in a form that we can easily store in a table, we'll ask the model to return the result in a string that reflects `JSON` representation, and be very specific of the schema that we expect
+-- MAGIC
+-- MAGIC Here's the prompt we've settled on:
+-- MAGIC ```
+-- MAGIC A customer left a review on a product. We want to follow up with anyone who appears unhappy.
+-- MAGIC Extract all entities mentioned. For each entity:
+-- MAGIC - classify sentiment as ["POSITIVE","NEUTRAL","NEGATIVE"]
+-- MAGIC - whether customer requires a follow-up: Y or N
+-- MAGIC - reason for requiring followup
+-- MAGIC
+-- MAGIC Return JSON ONLY. No other text outside the JSON. JSON format:
+-- MAGIC [{
+-- MAGIC     "product_name": <product name>,
+-- MAGIC     "category": <product category>,
+-- MAGIC     "sentiment": <review sentiment, one of ["POSITIVE","NEUTRAL","NEGATIVE"]>,
+-- MAGIC     "followup": <Y or N for follow up>,
+-- MAGIC     "followup_reason": <reason for followup>
+-- MAGIC }]
+-- MAGIC
+-- MAGIC Review:
+-- MAGIC <insert review text here>
+-- MAGIC ```
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Create the ANNOTATE function
+CREATE OR REPLACE FUNCTION ANNOTATE_REVIEW(review STRING)
+    RETURNS STRUCT<product_name: STRING, entity_sentiment: STRING, followup: STRING, followup_reason: STRING>
+    RETURN FROM_JSON(
+      ASK_LLM_MODEL(CONCAT(
+        'A customer left a review. We follow up with anyone who appears unhappy.
+         extract the following information:
+          - classify sentiment as ["POSITIVE","NEUTRAL","NEGATIVE"]
+          - returns whether customer requires a follow-up: Y or N
+          - if followup is required, explain what is the main reason
+
+        Return JSON ONLY. No other text outside the JSON. JSON format:
+        {
+            "product_name": <entity name>,
+            "entity_sentiment": <entity sentiment>,
+            "followup": <Y or N for follow up>,
+            "followup_reason": <reason for followup>
+        }
+        
+        Review:', review)),
+      "STRUCT<product_name: STRING, entity_sentiment: STRING, followup: STRING, followup_reason: STRING>")
+
+-- ALTER FUNCTION ANNOTATE_REVIEW OWNER TO `your_principal`; -- for the demo only, make sure other users can access your function
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Extract information from all our reviews
+CREATE OR REPLACE TABLE reviews_annotated as 
+    SELECT * EXCEPT (review_annotated), review_annotated.* FROM (
+      SELECT *, ANNOTATE_REVIEW(review) AS review_annotated
+        FROM fake_reviews LIMIT 10)
+    INNER JOIN fake_customers using (customer_id)
+
+-- COMMAND ----------
+
+SELECT * FROM reviews_annotated
+
+-- COMMAND ----------
+
+CREATE OR REPLACE FUNCTION GENERATE_RESPONSE(firstname STRING, lastname STRING, article_this_year INT, product STRING, reason STRING)
+  RETURNS STRING
+  RETURN ASK_LLM_MODEL(
+    CONCAT("Our customer named ", firstname, " ", lastname, " who ordered ", article_this_year, " articles this year was unhappy about ", product, 
+    "specifically due to ", reason, ". Provide an empathetic message I can send to my customer 
+    including the offer to have a call with the relevant product manager to leave feedback. I want to win back their 
+    favour and I do not want the customer to churn")
+  );
+-- ALTER FUNCTION GENERATE_RESPONSE OWNER TO `account users`; -- for the demo only, make sure other users can access your function
+
+-- COMMAND ----------
+
+SELECT GENERATE_RESPONSE("Quentin", "Ambard", 235, "Country Choice Snacking Cookies", "Quality issue") AS customer_response
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TABLE reviews_answer as 
+    SELECT *,
+      GENERATE_RESPONSE(firstname, lastname, order_count, product_name, followup_reason) AS response_draft
+    FROM reviews_annotated where followup='Y'
+    LIMIT 10
+
+-- COMMAND ----------
+
+SELECT * FROM reviews_answer
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Creating our Customer Review Dashboard
+-- MAGIC
+-- MAGIC The next step is to set up a comprehensive dashboard in order to track and monitor our customer reviews.
+-- MAGIC
+-- MAGIC Open the <a dbdemos-dashboard-id="customer-review-analysis" href='/sql/dashboardsv3/01ef714c9f511dc2b5b9f3b850714b22' target="_blank">Customer review analysis dashboard</a> to have a complete view of your customers, products and reviews.
+-- MAGIC
+-- MAGIC <img src="https://raw.githubusercontent.com/databricks-demos/dbdemos-resources/main/images/product/sql-ai-functions/sql-ai-function-dashboard.png" width="1200">
+-- MAGIC
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Going further
+-- MAGIC
+-- MAGIC Our pipeline is ready. Keep in mind that this is a fairly basic pipeline for our demo.
+-- MAGIC
+-- MAGIC For more advanced pipeline, we recommend using Delta Live Table. DLT simplify data ingetsion and transformation tasks with incremental load, materialized view and more advanced features. For more details, run `dbdemos.install_demo('dlt-loans')`
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Extra: AdHoc Queries
+-- MAGIC
+-- MAGIC Remember that analysts can always use the `ASK_LLM_MODEL()` function we created earlier to apply their own prompts to the data.
+-- MAGIC
+-- MAGIC As short example, let's write a query to extract all review about beverages:
+
+-- COMMAND ----------
+
+SELECT review_id,
+    BOOLEAN(ASK_LLM_MODEL(
+      CONCAT("Does this review discuss beverages? Answer boolean: 'true' or 'false' only, lowercase, no explanations or notes nor final dot. Review: ", review)
+    )) AS is_beverage_review,
+    review
+  FROM fake_reviews LIMIT 10
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## You're now ready to process your text using external LLM models!
+-- MAGIC
+-- MAGIC We've seen that the lakehouse provide advanced AI capabilities, not only you can leverage external LLM APIs, but you can also build your own LLM with Databricks GenAI applications!
+-- MAGIC For more details on creating your chatbot with the Lakehouse, run: `dbdemos.install('llm-rag-chatbot')`
+-- MAGIC
+-- MAGIC Go back to [the introduction]($./01-SQL-AI-Functions-Introduction)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Extra: configure an External Model Endpoint to leverage external providers (OpenAI, Anthropic...) 
+-- MAGIC
+-- MAGIC This demo was using one Databricks Foundation Model (pricing token-based).
+-- MAGIC
+-- MAGIC Your model endpoint can also be setup to use an external model such as OpenAI. Open [04-Extra-setup-external-model-OpenAI]($./04-Extra-setup-external-model-OpenAI) for more details.
+-- MAGIC
+-- MAGIC
+-- MAGIC Go back to [the introduction]($./00-SQL-AI-Functions-Introduction)
---
+++ sql-ai-functions/04-Extra-setup-external-model-OpenAI.py
@@ -0,0 +1,107 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Optional: Setup an external Model Serving Endpoint
+# MAGIC
+# MAGIC The Model Serving Endpoints used by your AI function can be of any type.
+# MAGIC
+# MAGIC You can setup an endpoint to hit an external Foundation Model, such as Azure OpenAI.
+# MAGIC
+# MAGIC This let you define your secret and connection setup once, and allow other users to access your external model, ensuring security, cost control and traceability.
+# MAGIC
+# MAGIC These can be of different flavor: Azure OpenAI, Anthropic, OpenAI...
+# MAGIC
+# MAGIC ## Setting up Azure OpenAI
+# MAGIC
+# MAGIC To be able to call our AI function, we'll need a key to query the Azure OpenAI API. Here are the steps to retrieve your key:
+# MAGIC
+# MAGIC <img src="https://raw.githubusercontent.com/databricks-demos/dbdemos-resources/main/images/product/sql-ai-functions/sql-ai-function-setup-1.png" width="1250px">
+# MAGIC
+# MAGIC - Navigate to [Azure Portal > All Services > Cognitive Services > Azure OpenAI](https://portal.azure.com/#view/Microsoft_Azure_ProjectOxford/CognitiveServicesHub/~/OpenAI)  
+# MAGIC - Click into the relevant resource
+# MAGIC - Click on `Keys and Endpoint`
+# MAGIC   - Copy down your:
+# MAGIC     - **Key** (do **NOT** store this in plaintext, include it in your code, and/or commit it into your git repo)
+# MAGIC     - **Resource name** which is represented in the endpoint URL (of the form `https://<resource-name>.openai.azure.com/`)
+# MAGIC - Click on `Model deployments`
+# MAGIC   - Note down your **model deployment name**
+# MAGIC
+# MAGIC <!-- Collect usage data (view). Remove it to disable collection or disable tracker during installation. View README for more details.  -->
+# MAGIC <img width="1px" src="https://ppxrzfxige.execute-api.us-west-2.amazonaws.com/v1/analytics?category=DBSQL&org_id=1444828305810485&notebook=%2F04-Extra-setup-external-model-OpenAI&demo_name=sql-ai-functions&event=VIEW&path=%2F_dbdemos%2FDBSQL%2Fsql-ai-functions%2F04-Extra-setup-external-model-OpenAI&version=1">
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Store your key using Databricks secrets
+# MAGIC
+# MAGIC We'll use [Databricks secrets](https://docs.databricks.com/security/secrets/index.html) to hold our API tokens. Use the [Databricks Secrets CLI](https://docs.databricks.com/dev-tools/cli/secrets-cli.html) or [Secrets API 2.0](https://docs.databricks.com/dev-tools/api/latest/secrets.html) to manage your secrets. The below examples use the Secrets CLI
+# MAGIC
+# MAGIC - If you don't already have a secret scope to keep your OpenAI keys in, create one now: 
+# MAGIC   `databricks secrets create-scope --scope dbdemos`
+# MAGIC - You will need to give `READ` or higher access for principals (e.g. users, groups) who are allowed to connect to OpenAI. 
+# MAGIC   - We recommend creating a group `openai-users` and adding permitted users to that group
+# MAGIC   - Then give that group `READ` (or higher) permission to the scope: 
+# MAGIC     `databricks secrets put-acl --scope dbdemos --principal openai-users --permission READ`
+# MAGIC - Create a secret for your API access token. We recommend format `<resource-name>-key`: 
+# MAGIC   `databricks secrets put --scope dbdemos --key azure-openai`
+# MAGIC
+# MAGIC **Caution**: Do **NOT** include your token in plain text in your Notebook, code, or git repo
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Create an external model endpoint
+# MAGIC To run the following code, connect this notebook to a <b> Standard interactive cluster </b>.
+
+# COMMAND ----------
+
+# MAGIC %pip install databricks-sdk==0.18.0 mlflow==2.9.0
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+from mlflow.deployments import get_deploy_client
+deploy_client = get_deploy_client("databricks")
+
+try:
+    endpoint_name  = "dbdemos-azure-openai"
+    deploy_client.create_endpoint(
+        name=endpoint_name,
+        config={
+            "served_entities": [
+                {
+                    "name": endpoint_name,
+                    "external_model": {
+                        "name": "gpt-35-turbo",
+                        "provider": "openai", #can support openAI or other external models. see https://docs.databricks.com/en/generative-ai/external-models/index.html
+                        "task": "llm/v1/chat",
+                        "openai_config": {
+                            "openai_api_type": "azure",
+                            "openai_api_key": "{{secrets/dbdemos/azure-openai}}", #Replace with your own azure open ai key
+                            "openai_deployment_name": "dbdemo-gpt35", #your deployment name
+                            "openai_api_base": "https://dbdemos-open-ai.openai.azure.com/", #your api base
+                            "openai_api_version": "2023-05-15"
+                        }
+                    }
+                }
+            ]
+        }
+    )
+    print(f"Endpoint {endpoint_name} created successfully")
+except Exception as e:
+    if 'RESOURCE_ALREADY_EXISTS' in str(e):
+        print('Endpoint already exists')
+    else:
+        raise e
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Your external model is now ready!
+# MAGIC
+# MAGIC You can use it in your SQL AI function:
+# MAGIC
+# MAGIC `SELECT AI_QUERY('dbdemos-azure-openai', 'This will be sent to azure openai!')``
+# MAGIC
+# MAGIC Congrats! You're now ready to use any model within your SQL queries!
+# MAGIC
+# MAGIC Go back to [the introduction]($./00-SQL-AI-Functions-Introduction)
